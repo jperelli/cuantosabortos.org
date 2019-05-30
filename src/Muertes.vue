a53f433ff75e72c51603b0185d846cba62d55de1
tomasdisk <tomasdisk@hotmail.com>
2019-05-30 00:47:27 -0300

    m4

--- src/Muertes.vue
+++ src/Muertes.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="muertes">
     <div>
-      <div class="title">2 muertes confirmadas</div>
+      <div class="title">4 muertes confirmadas</div>
       <br>
       <br>
     </div>
@@ -15,6 +15,10 @@
     </div>
     <div>
       <div class="caption"><a href="https://www.infobae.com/sociedad/2018/11/07/tiene-13-anos-esta-desnutrida-y-fue-mama-su-bebe-peso-menos-de-un-kilo-y-murio/">3. Niña Qom de 13 años, 2 de noviembre de 2018, Castelli (Chaco)</a></div>
+      <br>
+    </div>
+    <div>
+      <div class="caption"><a href="https://www.tiempoar.com.ar/nota/aborto-clandestino-murio-una-mujer-de-32-anos-en-el-hospital-de-pacheco">3. Mujer de 31 años, 27 de mayo de 2019, Tigre (Buenos Aires)</a></div>
       <br>
     </div>
     <div class="caption2">
